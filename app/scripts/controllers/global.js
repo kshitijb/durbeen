f093158f9615a199fb21e5e3afc4a1c2acb8ff93
Kshitij Bhardwaj <kshitij.bhardwaj@housing.com>
2015-07-02 22:16:57 +0530

    Dummy get request to receive csrf token from backend

--- app/scripts/controllers/global.js
+++ app/scripts/controllers/global.js
@@ -8,21 +8,28 @@
  * Controller of the durbeenApp
  */
 angular.module('durbeenApp')
-  .controller('GlobalCtrl', function ($scope, $location, loginService, $auth) {
-  	$scope.isLoggedIn = loginService.isAuthenticated;
-    $scope.login = function(){
-    	loginService.authenticate('slack')
-    	.then(function(){
-    		loginService.isAuthenticated = $auth.isAuthenticated();
-	    	$scope.isLoggedIn = loginService.isAuthenticated;
-    	});
-    };
-    $scope.logout = function(){
-    	loginService.logout();
-    	loginService.isAuthenticated = $auth.isAuthenticated();
-    	$scope.isLoggedIn = loginService.isAuthenticated;
-    	if (!$scope.isLoggedIn){
-    		$location.path('/login');
-    	}
-    };
-  });
+    .controller('GlobalCtrl', function($scope, $location, loginService, $auth, $cookies) {
+        $scope.isLoggedIn = loginService.isAuthenticated;
+        $scope.login = function() {
+            loginService.authenticate('slack')
+                .then(function() {
+                    loginService.isAuthenticated = $auth.isAuthenticated();
+                    $scope.isLoggedIn = loginService.isAuthenticated;
+                });
+        };
+        $scope.logout = function() {
+            loginService.logout();
+            loginService.isAuthenticated = $auth.isAuthenticated();
+            $scope.isLoggedIn = loginService.isAuthenticated;
+            if (!$scope.isLoggedIn) {
+                $location.path('/login');
+            }
+        };
+
+        if (!$cookies['csrftoken']) {
+            loginService.dummyGet()
+                .then(function(res) {
+                    console.log('got csrf token now!');
+                });
+        }
+    });
--- app/scripts/services/login.js
+++ app/scripts/services/login.js
@@ -8,13 +8,24 @@
  * Service in the durbeenApp.
  */
 angular.module('durbeenApp')
-  .service('loginService', function ($auth) {
-    // AngularJS will instantiate a singleton by calling "new" on this function
-    this.authenticate = function(provider){
-    	return $auth.authenticate(provider);
-    };
-    this.logout = function(){
-    	$auth.logout();
-    };
-    this.isAuthenticated = $auth.isAuthenticated();
-  });
+    .service('loginService', function($auth, globalVars, $http) {
+        // AngularJS will instantiate a singleton by calling "new" on this function
+        this.authenticate = function(provider) {
+            return $auth.authenticate(provider);
+        };
+        this.logout = function() {
+            $auth.logout();
+        };
+        this.isAuthenticated = $auth.isAuthenticated();
+
+        this.dummyGet = function() {
+            var url = globalVars.backendBaseUrl + 'api/dummy';
+            var promise = $http.get(url, {
+                    cache: true
+                })
+                .success(function(response) {
+                    return response;
+                });
+            return promise;
+        };
+    });
