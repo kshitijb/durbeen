d80df992beb36ee764c52fb16bc8216e243d5c40
Kushagra Agarwal <kushsolitary@gmail.com>
2015-07-03 13:12:46 +0530

    add mod-rewrite to make hashless url work with reloading

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,6 +15,8 @@ module.exports = function (grunt) {
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
 
+  var modRewrite = require('connect-modrewrite');
+
   // Configurable paths for the application
   var appConfig = {
     app: require('./bower.json').appPath || 'app',
@@ -72,9 +74,9 @@ module.exports = function (grunt) {
       },
       livereload: {
         options: {
-          open: true,
-          middleware: function (connect) {
+          middleware: function (connect, options) {
             return [
+              modRewrite(['^[^\\.]*$ /index.html [L]']),
               connect.static('.tmp'),
               connect().use(
                 '/bower_components',
